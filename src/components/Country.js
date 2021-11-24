049d8ca04396900f3a7a5e9d404512b7782cdc82
Branchey <77419938+Branchey@users.noreply.github.com>
2021-11-24 12:23:43 +0000

    added more country details and styling

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,8 +1,14 @@
+import './Country.css';
 
 const Country = ({country}) => {
     return (
         <div className="CountryDiv">
             <h2>{country.name.common}</h2>
+            <hr/>
+            <p>Capital: {country.capital}</p>
+            <p>Continent: {country.continents}</p>
+            <p>Population: {country.population.toLocaleString()}</p>
+            <p>Flag: <br/><img src={country.flags.png}></img></p>
 
         </div>
     )
